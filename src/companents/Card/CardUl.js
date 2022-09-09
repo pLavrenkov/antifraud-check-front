9d04cdc25b0c5136e22a15c371f4a2d413960eb1
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-09-09 17:42:39 +0300

    card click

--- src/companents/Card/CardUl.js
+++ src/companents/Card/CardUl.js
@@ -1,6 +1,10 @@
-function CardUl({inn, invalid, namec, namep, okved2, okved2name, periodcode, regionname, token, yearcode}) {
+function CardUl({ inn, invalid, namec, namep, okved2, okved2name, periodcode, regionname, token, yearcode }) {
+    const handleClick = () => {
+        console.log(token);
+    }
+    
     return (
-        <section className="card">
+        <section onClick={handleClick} className="card">
             <h3 className="card__title">{namec}</h3>
             <p className="card__field card__field_type_inn">{inn}</p>
             <p className="card__field card__field_type_region">{regionname}</p>
